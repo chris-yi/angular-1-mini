f3c99b6ee5b5e2b3739480e24e1cead4babe8a4d
chris-yi <kri5to.yi@gmail.com>
2017-11-14 12:28:45 -0700

    Black Diamond

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,14 @@
 angular.module("friendsList").controller("mainCtrl", function($scope){
     $scope.friends = [ "Garry", "Jerry", "Larry", "Perry", "Mary" ];
+
+    $scope.add = function(friendName) {
+        $scope.friends.push(friendName);
+        $scope.newFriend = "";
+    }
+
+    $scope.delete = function(name) {
+        let index = $scope.friends.indexOf(name)
+        $scope.friends.splice(index, 1)
+    }
 });
 
